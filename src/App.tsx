e357064fb34d57f422d298c2fa96fa2b629358ec
aluno <aluno@NV-LAB02-135501.ifms.edu.br>
2024-03-11 11:21:56 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//component React
+import './App.css'
+function App() {
+let nome = "Guilherme Vinícius "
+let sobrenome = "Pinheiro Menezes"
+  return (
+    <>
+      <h1 className="nome">Nome: {nome} </h1>
+      <p className="sobrenome">{sobrenome}</p>
+    </>
+  )
+}
+
+export default App
