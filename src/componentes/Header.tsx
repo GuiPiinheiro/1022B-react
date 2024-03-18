8e96186a1423ab886e86cb4f19bfa69289551c85
GuiPiinheiro <guilherme.menezes@estudante.ifms.edu.br>
2024-03-18 17:21:10 -0400

    Finalização do header

---
+++ src/componentes/Header.tsx
@@ -0,0 +1,24 @@
+import './Header.css'
+function Header(){
+    return(
+    <header>
+    <div className='logo'>
+        1022-B
+    </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href='https://'>Home</a>
+                </li>
+                <li>
+                    <a href='https://'>Listagem</a>
+                </li>
+                <li>
+                    <a href='https://'>Informações</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
+export default Header
---
+++ src/componentes/MeuComponente.tsx
@@ -0,0 +1,12 @@
+<body> </body>
+import './MeuComponente.css'
+
+const MeuComponente = () => {
+    return (
+        <header>
+           <div className="logo">logo</div>
+        </header>
+    );
+};
+
+export default MeuComponente;
